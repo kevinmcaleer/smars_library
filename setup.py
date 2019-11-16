8b9814bb89bd5dfc54809fbd7dd85827848cea87
Kevin McAleer <kevinmcaleer@me.com>
2019-11-16 14:26:21 +0000

    update version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="smars_library",
-    version="0.1.5",
+    version="0.1.6",
     description="SMARS Robot Python Library",
     long_description=README,
     long_description_content_type="text/markdown",
