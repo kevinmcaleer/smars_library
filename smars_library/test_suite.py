ba07ba9cf87325fa8fbb2bcfcccfd24c6fe9d130
Kevin McAleer <kevinmcaleer@gmail.com>
2020-03-21 19:09:26 +0000

    Updated tests

--- smars_library/test_suite.py
+++ smars_library/test_suite.py
@@ -1,3 +1,7 @@
+'''
+Unit tests for SMARS Library
+'''
+
 import unittest
 from constants import Channel
 
@@ -15,11 +19,17 @@ class SetServoPulseTestCase(unittest.TestCase):
     #     for channel in range(0, 16):
     #         self.assertTrue(set_servo_pulse(channel, 10))
 
-    def test_setservo_pulse_channel_less_than_15(self):  # Python 3 library test
+    def test_setservo_pulse_channel_less_than_15(self):
+        '''
+        Python 3 library test
+        '''
         for channel in range(0, 16):
             self.assertTrue(set_servo_pulse(channel, 10))
 
-    def testsetservopulseisnumnerbetween0and4096(self):  # Python 3 library test
+    def testsetservopulseisnumnerbetween0and4096(self):
+        '''
+        Tests the set servo pulse function
+        '''
         self.assertTrue(set_servo_pulse(0, 0))
         self.assertTrue(set_servo_pulse(0, 4096))
         self.assertTrue(set_servo_pulse(0, 2000))
@@ -27,25 +37,40 @@ class SetServoPulseTestCase(unittest.TestCase):
         self.assertFalse(set_servo_pulse(0, -1))
 
     def test_setangle3(self):  # Python 3 library test
-        legtest3 = Leg(channel=0, leg_minangle=0, leg_maxangle=180, invert=False, name="testbot")  # type: Leg
+        '''
+        Tests the angle constraints
+        '''
+        legtest3 = Leg(channel=0,
+                       leg_minangle=0,
+                       leg_maxangle=180,
+                       invert=False,
+                       name="testbot")
         self.assertTrue(legtest3.setangle(0))
         self.assertTrue(legtest3.setangle(180))
         self.assertFalse(legtest3.setangle(181))
 
 class TestLegSetdefault(unittest.TestCase):
-    """ tests leg setdefault() """
+    '''
+    tests leg setdefault()
+    '''
 
     def test_setdefault(self):
-        l = Leg(channel=0, leg_minangle=0, leg_maxangle=180, invert=False, name="testbot")
-        self.assertIsNone(l.setdefault())
+        '''
+        tests set default
+        '''
+        leg = Leg(channel=0, leg_minangle=0, leg_maxangle=180, invert=False, name="testbot")
+        self.assertIsNone(leg.setdefault())
 
 
 class TestLegSetBody(unittest.TestCase):
     """ tests leg setbody() """
 
     def test_setbody(self):
-        l = Leg(channel=0, leg_minangle=0, leg_maxangle=180, invert=False, name="testbot")
-        self.assertIsNone(l.setbody())
+        '''
+        test SetBody function
+        '''
+        leg = Leg(channel=0, leg_minangle=0, leg_maxangle=180, invert=False, name="testbot")
+        self.assertIsNone(leg.setbody())
 
 class TestConstants(unittest.TestCase):
     """ tests constants.py """
